7149e1d5b19d329320fd9cde62e0cd2fb5a2c19f
Mike Olivieri <molivieri@articulate.com>
2011-10-02 20:19:17 -0400

    adding ability to expand screenr

--- content.js
+++ content.js
@@ -12,12 +12,64 @@ jQuery.expr[':'].regex = function(elem, index, match) {
     return regex.test(jQuery(elem)[attr.method](attr.property));
 }
 
+$(window).resize($.debounce(500, function() {
+
+	var screencasts = $("iframe:regex(src, viewscreencasts.com)");
+
+	for (var i=0;i < screencasts.length; i++) {
+		resizeScreenr(screencasts[i]);
+	}
+	
+
+}));
+
+$(".expand_screenr").live('click', function(e) {
+	
+	var el = $(e.target);
+	//get screenr
+	var screenr = $("#" + el.data('screenr'));
+	
+	if (screenr.data('expand') === false) {
+		el.text('Minimize Screenr');
+		screenr.data("expand", true);
+		resizeScreenr(screenr);
+		return false;
+	}
+	
+	el.text('Expand Screenr');
+	screenr.data("expand", false);
+	screenr.height(396);
+	screenr.width(650);
+	return false;	
+	
+});
+
+function resizeScreenr(screenr) {
+	
+	if ($(screenr).data("expand") === true) {
+		
+		var newWidth = (window.innerWidth - 30 )- $(screenr).position().left
+		var ratio = newWidth / $(screenr).width();
+		$(screenr).width(newWidth);
+		$(screenr).height($(screenr).height() * ratio);   
+	}
+
+}
+
 function insertScreenrEmbed(link) {
    
+    var id = new Date().getTime();
+   
     var src = $(link).attr('href');
-	var iframe = $("<iframe width='650' height='396' frameborder='0'></iframe>");
+	var iframe = $("<iframe data-expand='false'  id='" + id +"' width='650' height='396' frameborder='0'></iframe>");
 	iframe.attr("src", src.replace('.com/', '.com/embed/'));
-	iframe.insertAfter(link);
+	iframe.insertBefore(link);
+    
+    var div = $("<div></div>");
+	div.insertBefore(iframe);
+	
+    var expand = $('<a style="font-size:10pt" href="#" data-screenr="' + id + '" class="expand_screenr">Expand Screenr</a>');
+	expand.appendTo(div);
 }
 
 if (window.location.href.indexOf(".viewscreencasts.com") == -1) {
